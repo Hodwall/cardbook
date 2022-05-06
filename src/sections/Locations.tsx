9f54491e4e9577bc4c3350a147b1a9056d4144f5
Manel Escandell <manel.escandell@ingenico.com>
2022-05-06 13:56:52 +0200

    Adds Under Construction display for unfinished sections

--- src/sections/Locations.tsx
+++ src/sections/Locations.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import OptionsBar from '../containers/OptionsBar';
+import Blacksmith from '../img/blacksmith.png';
 
 const Locations = () => {
     return (
         <React.Fragment>
             <OptionsBar></OptionsBar>
+            <div className="bg-message">SECTION BEING BUILT</div>
+            <img className="bg-icon" src={Blacksmith} />
         </React.Fragment>
     )
 }
